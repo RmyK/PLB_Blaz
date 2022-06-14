ec75b7ed7885e51cd7ac1fdfc33cafd23f40fbba
Remy <remy.kreder@devconception.fr>
2022-06-14 10:09:14 +0200

    Mardi 10H

--- ToDoListBlazor/Pages/ToDoPage.razor.cs
+++ ToDoListBlazor/Pages/ToDoPage.razor.cs
@@ -16,17 +16,40 @@ namespace ToDoListBlazor.Pages
         private List<ToDo> ToDos;
         private List<ToDo> TodoTobedone => ToDos.Where(t => !t.IsDone).ToList();
         private List<ToDo> TodoDone => ToDos.Where(t => t.IsDone).ToList();
-
+        private ToDo toDoToAddOrEdit;
 
         protected override void OnInitialized()
         {
             ToDos = toDoService.GetTodos().ToList();
         }
 
+        private void AjouterTodo()
+        {
+            toDoToAddOrEdit = new ToDo();
+        }
+
+        private void EditTodo(ToDo todo)
+        {
+            toDoToAddOrEdit = todo;
+        }
+
         private void ChangeTodoStatus(int id)
         {
             var todo = ToDos.SingleOrDefault(t => t.Id == id);
             todo.IsDone = !todo.IsDone;
+        }
+
+        private void SaveTodo()
+        {
+            if (toDoToAddOrEdit.Id == 0)
+            {
+                toDoService.AddTodo(toDoToAddOrEdit);
+                ToDos.Add(toDoToAddOrEdit);
+            }
+            else
+                toDoService.UpdateTodo(toDoToAddOrEdit);
+
+            toDoToAddOrEdit = null;
         }
     }
 }
