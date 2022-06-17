5c64e8f2ea8909b1aac386f90df06b9a490f040c
Remy <remy.kreder@devconception.fr>
2022-06-17 15:01:25 +0200

    upd vendredi 15h

---
+++ ToDoListWASM/ToDoListWASM/Server/Controllers/TodoController.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.DataModels;
+using DataAccess.Services;
+using Microsoft.AspNetCore.Mvc;
+using ToDoListWASM.Shared.DTOs;
+
+namespace ToDoListWASM.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ToDoService todoSrv;
+        public TodoController(ToDoService toDoService)
+        {
+            todoSrv = toDoService;
+        }
+
+        //[HttpGet]
+        //[Route("getall")]
+        [HttpGet("getall")]
+        public IEnumerable<TodoDTO> GetAllTodos()
+        {
+            return todoSrv.GetTodos().Select(t =>
+                new TodoDTO()
+                {
+                    Id = t.Id,
+                    IsDone = t.IsDone,
+                    Order = t.Order,
+                    TodoLabel = t.TodoLabel,
+                    UserId = t.UserId
+                });
+        }
+
+        [HttpPost]
+        public IActionResult InsertTodo(TodoDTO dTO)
+        {
+            try
+            {
+                todoSrv.AddTodo(new ToDo()
+                {
+                    Id = dTO.Id,
+                    IsDone = dTO.IsDone,
+                    Order = dTO.Order,
+                    TodoLabel = dTO.TodoLabel,
+                    UserId = dTO.UserId
+                });
+
+                return Ok(true);
+            }
+            catch (Exception)
+            {
+                return Problem(statusCode: 999, detail: "message d'erreur custom");
+            }
+        }
+    }
+}
---
+++ ToDoListWASM/ToDoListWASM/Shared/DTOs/TodoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListWASM.Shared.DTOs
+{
+    public class TodoDTO
+    {
+        public int Id { get; set; }
+        public int Order { get; set; }
+        public string TodoLabel { get; set; }
+        public bool IsDone { get; set; }
+        public int UserId { get; set; }
+    }
+}
